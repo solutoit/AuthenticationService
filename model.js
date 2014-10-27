ecca8769ea4d562fd09a8deaa53eb16190fa4f59
asafkotzer <asafkotzer@gmail.com>
2014-10-27 09:02:47 +0200

    First commit - added resource owner credentials flow (password grant type) as a demo

---
+++ model.js
@@ -0,0 +1,92 @@
+//reference: https://github.com/thomseddon/node-oauth2-server/blob/master/examples/postgresql/model.js
+
+var model = module.exports,
+  connString = process.env.DATABASE_URL;
+
+var accessTokensByBearerToken = {};
+var clientsByClientId = {};
+var usersByUsername = {};
+
+(function () {
+    accessTokensByBearerToken["dummy"] = {
+        access_token: "dummy_access_token",
+        client_id: "dummy_client_id",
+        expires: "dummy_expires",
+        userId: "dummy_userId",
+    };
+
+    clientsByClientId["dummy_client_id"] = {
+        client_id: "dummy_client_id",
+        client_secret: "dummy_client_secret",
+    };
+
+    usersByUsername["asaf"] = {
+        username: "asaf",
+        password: "po09!@QW"
+    };
+}());
+
+model.getAccessToken = function (bearerToken, callback) {
+    console.log("getAccessToken");
+
+    var token = accessTokensByBearerToken[bearerToken];
+    callback(null, {
+        accessToken: token.access_token,
+        clientId: token.client_id,
+        expires: token.expires,
+        userId: token.userId
+    });
+};
+
+model.getClient = function (clientId, clientSecret, callback) {
+    var client = clientsByClientId[clientId];
+    console.log("getClient: " + client.client_id);
+
+    callback(null, {
+        clientId: client.client_id,
+        clientSecret: client.client_secret
+    });
+};
+
+model.getRefreshToken = function (bearerToken, callback) {
+    console.log("not implemented");
+    callback(null, false);
+};
+
+var authorizedClientIds = ['dummy_client_id', 'asaf_client_id'];
+model.grantTypeAllowed = function (clientId, grantType, callback) {
+    console.log("grantTypeAllowed");
+
+    if (grantType === 'password') {
+        return callback(false, authorizedClientIds.indexOf(clientId.toLowerCase()) >= 0);
+    }
+
+    callback(false, true);
+};
+
+model.saveAccessToken = function (accessToken, clientId, expires, userId, callback) {
+    console.log("saveAccessToken");
+
+    accessTokensByBearerToken[accessToken] = {
+        accessToken: accessToken,
+        clientId: clientId,
+        expires: expires,
+        userId: userId
+    };
+    callback(null);
+};
+
+model.saveRefreshToken = function (refreshToken, clientId, expires, userId, callback) {
+    console.log("not implemented");
+    callback(null);
+};
+
+/*
+ * Required to support password grant type
+ */
+model.getUser = function (username, password, callback) {
+    console.log("getUser");
+
+    var user = usersByUsername[username];
+    callback(null, user && user.password == password);
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    oauthserver = require('oauth2-server'),
+    model = require('./model.js');
+
+var app = express();
+
+app.use(bodyParser()); // REQUIRED
+
+app.oauth = oauthserver({
+    model: model,
+    grants: ['password'],
+    debug: true
+});
+
+app.all('/oauth/token', app.oauth.grant());
+
+app.get('/', app.oauth.authorise(), function (req, res) {
+    res.send('Secret area');
+});
+
+app.use(app.oauth.errorHandler());
+
+app.listen(3000);
